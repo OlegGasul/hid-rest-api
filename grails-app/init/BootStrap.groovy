79773b0514ef1f737529466d776ac48a803bb4eb
oleg <oleg.gasul@gmail.com>
2016-10-04 21:52:18 +0300

    Add test data for development environment

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -3,19 +3,22 @@ import com.hid.api.model.Data
 import com.hid.api.security.AppUser
 import com.hid.api.security.AppUserRole
 import com.hid.api.security.Role
+import grails.util.Environment
 
 class BootStrap {
 
     def init = { servletContext ->
-//        def userRole = Role.findOrSaveByAuthority('ROLE_USER')
-//        def testUser = AppUser.findOrSaveByUsernameAndPassword('root', 'se3ret123')
-//
-//        AppUserRole.create testUser, userRole
-//
-//        AppUserRole.withSession {
-//            it.flush()
-//            it.clear()
-//        }
+        if (Environment.current == Environment.DEVELOPMENT) {
+            def userRole = Role.findOrSaveByAuthority('ROLE_USER')
+            def testUser = AppUser.findOrSaveByUsernameAndPassword('root', 'se3ret123')
+
+            AppUserRole.create testUser, userRole
+
+            AppUserRole.withSession {
+                it.flush()
+                it.clear()
+            }
+        }
     }
 
     def destroy = {
