d7d3c0ab3c3e9c13c6202d0dc7645b9610b5ddd5
oleg <oleg.gasul@gmail.com>
2016-10-04 14:25:37 +0300

    Init script

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -7,19 +7,15 @@ import com.hid.api.security.Role
 class BootStrap {
 
     def init = { servletContext ->
-//        def userRole = Role.findOrSaveByAuthority('ROLE_USER')
-//        def testUser = AppUser.findOrSaveByUsernameAndPassword('root', 'se3ret123')
-//
-//        AppUserRole.create testUser, userRole
-//
-//        AppUserRole.withSession {
-//            it.flush()
-//            it.clear()
-//        }
-//
-//        def clazz = Clazz.findOrSaveByName("ARTWORK".toLowerCase())
-//        def data = new Data(clazz: clazz, key: "PN0001".toLowerCase(), value: '{test: "test"}', contentLength: 14, contentType: 'application/json')
-//        data.save(failOnError: true, flush: true)
+        def userRole = Role.findOrSaveByAuthority('ROLE_USER')
+        def testUser = AppUser.findOrSaveByUsernameAndPassword('root', 'se3ret123')
+
+        AppUserRole.create testUser, userRole
+
+        AppUserRole.withSession {
+            it.flush()
+            it.clear()
+        }
     }
 
     def destroy = {
